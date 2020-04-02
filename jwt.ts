77ab670209e5ce197b443b45f29034b1c0846e7e
Andrea Coradi <andreacoradi30@gmail.com>
2020-04-02 20:34:34 +0200

    Fixed to support breaking changes in JWT API

--- jwt.ts
+++ jwt.ts
@@ -1,18 +1,21 @@
-import makeJwt, { setExpiration } from "https://deno.land/x/djwt/create.ts";
+import makeJwt, {
+  setExpiration,
+  Jose,
+  Payload } from "https://deno.land/x/djwt/create.ts";
 import validateJwt from "https://deno.land/x/djwt/validate.ts";
 
 const env = Deno.env();
 
 export const create = async (username: string) => {
-  const claims = {
+  const claims: Payload = {
     iss: username,
     exp: setExpiration(new Date().getTime() + 60 * 60 * 1000)
   };
-  const header = {
+  const header: Jose = {
     alg: "HS512",
     typ: "JWT"
   };
-  return makeJwt(header, claims, env.SALT);
+  return makeJwt({header: header, payload: claims}, env.SALT);
 };
 
 export const validate = async (token: string) => {
